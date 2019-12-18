d0e5dd8da5b15185b889a9227b6098abacf8b7e6
Jiangqiang <JiangQiangEmail@163.com>
2019-12-18 19:49:21 +0800

    新建项目1

--- src/main/java/com/jq/mt_data_view/controler/Test.java
+++ src/main/java/com/jq/mt_data_view/controler/Test.java
@@ -1,4 +1,4 @@
-package com.jq.mt_data_view.common;
+package com.jq.mt_data_view.controler;
 
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
